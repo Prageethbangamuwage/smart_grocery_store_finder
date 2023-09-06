626bc6ec0199eff62b3bda6271f3214ded52b8c3
KumudikaWijesinghe <kumudikahansini128@gmail.com>
2023-09-06 18:26:36 +0530

    comment

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,19 +1,21 @@
-import 'package:flutter/material.dart' ;
+import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("HomePage", style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          color: Colors.blue,
-        ),),
+        child: Text(
+          "HomePage",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue, //check
+          ),
+        ),
       ),
     );
-
   }
 }
